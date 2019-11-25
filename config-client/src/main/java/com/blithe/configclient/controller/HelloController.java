4f5d5d3ea432c59b91f1419c8d5a5e1454a06bfb
BlitheXie <957474413@qq.com>
2019-11-26 00:35:48 +0800

    config-client

---
+++ config-client/src/main/java/com/blithe/configclient/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.blithe.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * @Author: xieyongning
+ * @Date: 2019/11/25 23:30
+ * @Version 1.0
+ */
+@RestController
+public class HelloController {
+
+    @Value("${neo.hello:error}")
+    private String profile;
+
+    @GetMapping("/info")
+    public Mono<String> hello(){
+        return Mono.justOrEmpty(profile);
+    }
+}
