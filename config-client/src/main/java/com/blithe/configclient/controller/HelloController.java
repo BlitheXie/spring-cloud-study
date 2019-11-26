55a825a66cb3dcc571aa59c983b054fbd5b1d384
BlitheXie <957474413@qq.com>
2019-11-26 23:38:58 +0800

    修改client

--- config-client/src/main/java/com/blithe/configclient/controller/HelloController.java
+++ config-client/src/main/java/com/blithe/configclient/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.blithe.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -11,6 +12,7 @@ import reactor.core.publisher.Mono;
  * @Version 1.0
  */
 @RestController
+@RefreshScope
 public class HelloController {
 
     @Value("${neo.hello:error}")
